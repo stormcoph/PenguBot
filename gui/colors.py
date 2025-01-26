787a90117158a13609ed044989cd0c1b71c94cbf
stormcoph <mr.capyyy@gmail.com>
2025-01-26 19:35:22 +0100

    delete stuff

--- gui/colors.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtGui import QColor
-
-class Colors:
-    BACKGROUND = QColor("#1A1B26")
-    PRIMARY = QColor("#7AA2F7")
-    SECONDARY = QColor("#414868")
-    ACCENT = QColor("#BB9AF7")
-    TEXT = QColor("#C0CAF5")
-    ERROR = QColor("#F7768E")
